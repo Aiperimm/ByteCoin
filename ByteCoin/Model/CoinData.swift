b008d0386e067b8b1f5da675b06f576db922b12e
AIPERIM AKISHEVA <AIPERIM1995@GMAIL.COM>
2022-02-24 17:33:08 -0500

    update User Interface using the Delegate Pattern We did Refactor with Extensions

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,16 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Aiperim Akisheva on 2/19/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+    
+}
+
+
